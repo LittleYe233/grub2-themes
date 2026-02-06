6975154fd4b16c5b0ea4cc5139b262a5ba27d535
Zhifan Ye <littleye233@gmail.com>
2026-02-06 13:34:58 +0800

    Use LXGW font and add compatibility for Arch Linux in `makefont.sh`

--- common/makefont.sh
+++ common/makefont.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/env bash
 
-grub2-mkfont -o unifont-16.pf2 -s 16 unifont.otf
-grub2-mkfont -o unifont-24.pf2 -s 24 unifont.otf
-grub2-mkfont -o unifont-32.pf2 -s 32 unifont.otf
+GRUB_MKFONT=grub2-mkfont
+
+# Some distros use other filenames
+_release=$(grep ID /etc/os-release | head -n 1 | sed 's/ID=//')
+if [[ $_release == "arch" ]]; then
+  GRUB_MKFONT=grub-mkfont
+fi
+
+$GRUB_MKFONT -o lxgw-16.pf2 -s 16 LXGWWenKaiMono-Regular.ttf
+$GRUB_MKFONT -o lxgw-24.pf2 -s 24 LXGWWenKaiMono-Regular.ttf
+$GRUB_MKFONT -o lxgw-32.pf2 -s 32 LXGWWenKaiMono-Regular.ttf
